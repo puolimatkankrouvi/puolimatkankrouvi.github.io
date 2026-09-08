fc829e016590fa3426441eb84037b69a8750a090
Henri Vekuna <cde_1414@yahoo.com>
2026-09-08 19:55:14 +0300

    Fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-    site: "https://github.io/puolimatkankrouvi",
+    site: "https://puolimatkankrouvi.github.io",
     output: "static"
 });
